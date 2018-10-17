e215023a29381a264974bd9b976479d83a619407
wkcool <wk343321@126.com>
2018-10-17 15:31:46 +0800

    add

--- lib/src/main/java/com/sirit/android/support/lib/extention/ActivityExtention.kt
+++ lib/src/main/java/com/sirit/android/support/lib/extention/ActivityExtention.kt
@@ -2,7 +2,10 @@ package com.sirit.android.support.lib.extention
 
 import android.app.Activity
 import android.support.v7.widget.Toolbar
+import android.util.TypedValue
 import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.sirit.android.support.lib.R
 import com.sirit.android.support.lib.widget.help.ViewTitleHelp
 
@@ -14,17 +17,32 @@ import com.sirit.android.support.lib.widget.help.ViewTitleHelp
 fun Activity.replaceToolbarLayout(toolbar: Toolbar, viewTitleHelp: ViewTitleHelp) {
     layoutInflater.inflate(R.layout.view_title, toolbar)
 
-    val leftTitle = findViewById<View>(R.id.leftTitle)
+    val leftTitle = findViewById<TextView>(R.id.leftTitle)
     val centerLayout = findViewById<View>(R.id.centerLayout)
-    val centerLeftTitle = findViewById<View>(R.id.centerLeftTitle)
-    val subTitle = findViewById<View>(R.id.subTitle)
-    val centerTitle = findViewById<View>(R.id.centerTitle)
-    val rightTitle = findViewById<View>(R.id.rightTitle)
+    val centerLeftTitle = findViewById<TextView>(R.id.centerLeftTitle)
+    val subTitle = findViewById<TextView>(R.id.subTitle)
+    val centerTitle = findViewById<TextView>(R.id.centerTitle)
+    val rightTitle = findViewById<TextView>(R.id.rightTitle)
 
     toolbar.layoutParams.height = viewTitleHelp.mTitleHeight
 
 
     leftTitle?.apply {
+        viewTitleHelp.mTitleLeftData?.let {
+
+            it.titleText?.let {
+                leftTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, it.textSize.toFloat())
+                leftTitle.setTextColor(color(it.textColor))
+
+                (leftTitle.layoutParams as? RelativeLayout.LayoutParams?)
+                    ?.setMargins(it.margin.left, it.margin.top, it.margin.right, it.margin.bottom)
+            }
+            it.titleIcon?.let {
+                leftTitle.layoutParams.width = it.width
+                leftTitle.layoutParams.height = it.height
+                leftTitle.background = mipmapDrawable(it.iconRes)
+            }
+        }
 
     }
 
--- lib/src/main/java/com/sirit/android/support/lib/widget/help/ViewTitleHelp.kt
+++ lib/src/main/java/com/sirit/android/support/lib/widget/help/ViewTitleHelp.kt
@@ -1,19 +1,57 @@
 package com.sirit.android.support.lib.widget.help
 
+import android.graphics.Rect
+import java.io.Serializable
+
 /**
  * @author kai.w
  * @des  $des
  */
-class ViewTitleHelp(viewTitleBuild: ViewTitleBuild) {
-    val mTitleHeight: Int by lazy { viewTitleBuild.mTitleHeight }
+class ViewTitleHelp(builder: ViewTitleBuilder) {
+    val mTitleHeight: Int by lazy { builder.mTitleHeight }
+    val mTitleLeftData: TitleData? by lazy { builder.mTitleLeftData }
+    val mTitleCenterLeftData: TitleData? by lazy { builder.mTitleCenterLeftData }
+    val mTitleSubData: TitleData? by lazy { builder.mTitleSubData }
+    val mTitleCenterData: TitleData? by lazy { builder.mTitleCenterData }
+    val mTitleRightData: TitleData? by lazy { builder.mTitleRightData }
 
     companion object {
-        class ViewTitleBuild {
+        class ViewTitleBuilder {
             var mTitleHeight: Int = 0
+            var mTitleLeftData: TitleData? = null
+            var mTitleCenterLeftData: TitleData? = null
+            var mTitleSubData: TitleData? = null
+            var mTitleCenterData: TitleData? = null
+            var mTitleRightData: TitleData? = null
 
-
-            fun setTitleHeight(titleHeight: Int): ViewTitleBuild {
+            fun setTitleHeight(titleHeight: Int): ViewTitleBuilder {
                 this.mTitleHeight = titleHeight
+                return this
+            }
+
+            fun setTitleLeftData(titleLeftData: TitleData): ViewTitleBuilder {
+                this.mTitleLeftData = titleLeftData
+                return this
+            }
+
+            fun setTitleCenterLeftData(titleCenterLeftData: TitleData): ViewTitleBuilder {
+                this.mTitleCenterLeftData = titleCenterLeftData
+                return this
+            }
+
+            fun setTitleSubData(titleSubData: TitleData): ViewTitleBuilder {
+                this.mTitleSubData = titleSubData
+                return this
+            }
+
+
+            fun setTitleCenterData(titleCenterData: TitleData): ViewTitleBuilder {
+                this.mTitleCenterData = titleCenterData
+                return this
+            }
+
+            fun setTitleRightData(titleRightData: TitleData): ViewTitleBuilder {
+                this.mTitleRightData = titleRightData
                 return this
             }
 
@@ -22,11 +60,12 @@ class ViewTitleHelp(viewTitleBuild: ViewTitleBuild) {
         }
 
 
-//        data class View
+        data class TitleData(val titleText: TitleText? = null, val titleIcon: TitleIcon? = null) : Serializable
 
-        enum class TitleType {
-            TEXT, ICON
-        }
+        data class TitleText(val textSize: Int, val textColor: Int, val margin: Rect) : Serializable
+
+        data class TitleIcon(val iconRes: Int, val width: Int, val height: Int, val margin: Rect) : Serializable
+
     }
 
 }
