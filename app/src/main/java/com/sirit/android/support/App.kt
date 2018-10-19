0035d7dd5ba3a76d2b1be07319074ee275c2a58a
wkcool <wk343321@126.com>
2018-10-19 16:47:47 +0800

    add

--- app/src/main/java/com/sirit/android/support/App.kt
+++ app/src/main/java/com/sirit/android/support/App.kt
@@ -1,7 +1,5 @@
 package com.sirit.android.support
 
-import com.facebook.FacebookSdk
-import com.facebook.appevents.AppEventsLogger
 import com.sirit.android.support.lib.BasicApp
 
 /**
@@ -13,7 +11,7 @@ class App : BasicApp() {
     override fun onCreate() {
         super.onCreate()
 
-        FacebookSdk.sdkInitialize(applicationContext)
-        AppEventsLogger.activateApp(this)
+//        FacebookSdk.sdkInitialize(applicationContext)
+//        AppEventsLogger.activateApp(this)
     }
 }
--- app/src/main/java/com/sirit/android/support/MainActivity.kt
+++ app/src/main/java/com/sirit/android/support/MainActivity.kt
@@ -1,64 +1,57 @@
 package com.sirit.android.support
 
-import android.net.Uri
-import android.os.Bundle
-import android.widget.Toast
-import com.facebook.share.model.ShareLinkContent
-import com.facebook.share.widget.ShareDialog
-import com.sirit.android.support.login.ThirdLoginActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : ThirdLoginActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private val EMAIL = "email"
-    private val USER_POSTS = "user_posts"
-    private val PUBLIC_PROFILE = "public_profile"
-    private val AUTH_TYPE = "rerequest"
-    private val shareDialog by lazy { ShareDialog(this) }
-
-    override fun onSuccess(result: com.facebook.login.LoginResult?) {
-        runOnUiThread {
-            Toast.makeText(this, result?.accessToken.toString()
-                ?: "success", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onCancel() {
-        runOnUiThread {
-            Toast.makeText(this, "onCancel", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onError(error: com.facebook.FacebookException?) {
-        runOnUiThread {
-            Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        // Set the initial permissions to request from the user while logging in
-        login_button.setReadPermissions(Arrays.asList(EMAIL, USER_POSTS, PUBLIC_PROFILE))
-        login_button.authType = AUTH_TYPE
-//        login_button.setReadPermissions("email")
-        // If using in a fragment
-//        login_button.setFragment(this)
-
-        // Callback registration
-        login_button.registerCallback(callbackManager, this)
-
-        btShare.setOnClickListener {
-            val content = ShareLinkContent.Builder()
-                .setContentUrl(Uri.parse("https://developers.facebook.com"))
-                .build()
-            shareDialog.show(content, ShareDialog.Mode.AUTOMATIC)
-        }
-
-//        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
-
-    }
+//    private val EMAIL = "email"
+//    private val USER_POSTS = "user_posts"
+//    private val PUBLIC_PROFILE = "public_profile"
+//    private val AUTH_TYPE = "rerequest"
+//    private val shareDialog by lazy { ShareDialog(this) }
+//
+//    override fun onSuccess(result: com.facebook.login.LoginResult?) {
+//        runOnUiThread {
+//            Toast.makeText(this, result?.accessToken.toString()
+//                ?: "success", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    override fun onCancel() {
+//        runOnUiThread {
+//            Toast.makeText(this, "onCancel", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    override fun onError(error: com.facebook.FacebookException?) {
+//        runOnUiThread {
+//            Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//
+//        // Set the initial permissions to request from the user while logging in
+//        login_button.setReadPermissions(Arrays.asList(EMAIL, USER_POSTS, PUBLIC_PROFILE))
+//        login_button.authType = AUTH_TYPE
+////        login_button.setReadPermissions("email")
+//        // If using in a fragment
+////        login_button.setFragment(this)
+//
+//        // Callback registration
+//        login_button.registerCallback(callbackManager, this)
+//
+//        btShare.setOnClickListener {
+//            val content = ShareLinkContent.Builder()
+//                .setContentUrl(Uri.parse("https://developers.facebook.com"))
+//                .build()
+//            shareDialog.show(content, ShareDialog.Mode.AUTOMATIC)
+//        }
+//
+////        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
+//
+//    }
 }
