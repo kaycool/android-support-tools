408f7e7375ee329c57f4687d18e5a3c81004873c
wkcool <wk343321@126.com>
2018-12-17 15:37:01 +0800

    add network xml

--- lib/src/main/java/com/sirit/android/support/lib/network/ResultBean.kt
+++ lib/src/main/java/com/sirit/android/support/lib/network/ResultBean.kt
@@ -12,49 +12,5 @@ class ResultBean<T> {
 
     companion object {
 
-        /**
-         * 接口正常
-         */
-        val RESULT_CODE_SUCCESS = 0
-        /**
-         * 网络异常
-         */
-        val RESULT_CODE_NET_ERROR = 111
-        /**
-         * 服务器错误
-         */
-        val RESULT_CODE_SERVER_ERROR = -1
-        /**
-         * 用户被挤掉
-         */
-        val RESULT_CODE_OTHER_LOGIN = 402
-        /**
-         * token过期状态
-         */
-        val RESULT_CODE_NO_LOGIN = 403
-        /**
-         * 资源没有发现
-         */
-        val RESULT_CODE_NO_FOUND = 404
-        /**
-         * 礼包过期
-         */
-        val RESULT_CODE_GIFT_TIME_OUT = 1024
-        /**
-         * 礼包重复领取
-         */
-        val RESULT_CODE_REPEAT_DATA = 1022
-        /**
-         * 礼包领取失败
-         */
-        val RESULT_CODE_GIFT_RECEIVER_ERROR = 1025
-        /**
-         * 礼包领取等级不够
-         */
-        val RESULT_CODE_NOT_PERMISSION = 1023
-        /**
-         * 晒单审核通过
-         */
-        val RESULT_CODE_SHOW_ORDER_SUCCEED = 1019
     }
 }
