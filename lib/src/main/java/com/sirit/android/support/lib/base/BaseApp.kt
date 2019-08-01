73a1281ea81731822170076689467b9469774c08
wangkai <wk343321@126.com>
2019-08-01 23:57:27 +0800

    add

--- lib/src/main/java/com/sirit/android/support/lib/base/BaseApp.kt
+++ lib/src/main/java/com/sirit/android/support/lib/base/BaseApp.kt
@@ -3,12 +3,28 @@ package com.sirit.android.support.lib.base
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
 
 /**
  * @author kai.w
  * @des  $des
  */
-open class BaseApp : Application() {
+open class BaseApp : Application(), KodeinAware {
+
+    private val appModule = Kodein.Module("APP") {
+        bind<Application>() with singleton { BaseApp() }
+    }
+
+    override val kodein by Kodein.lazy {
+        /* bindings */
+        import(appModule)
+        import(androidXModule(this@BaseApp))
+    }
+
 
     override fun onCreate() {
         super.onCreate()
