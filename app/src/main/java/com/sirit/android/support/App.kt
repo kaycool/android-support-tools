1967bb00f4fd862521a17fd7fe14bcbd937c0b92
wangkai <wk343321@126.com>
2019-08-04 22:24:14 +0800

    add

--- app/src/main/java/com/sirit/android/support/App.kt
+++ app/src/main/java/com/sirit/android/support/App.kt
@@ -1,12 +1,12 @@
 package com.sirit.android.support
 
-import com.sirit.android.support.lib.BasicApp
+import com.sirit.android.support.lib.base.BaseApp
 
 /**
  * @author kai.w
  * @des  $des
  */
-class App : BasicApp() {
+class App : BaseApp() {
 
     override fun onCreate() {
         super.onCreate()
--- lib/src/main/java/com/sirit/android/support/lib/base/BaseActivity.kt
+++ lib/src/main/java/com/sirit/android/support/lib/base/BaseActivity.kt
@@ -1,34 +1,20 @@
 package com.sirit.android.support.lib.base
 
-import android.arch.lifecycle.Lifecycle
-import android.arch.lifecycle.LifecycleOwner
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
-import android.view.WindowManager
-import android.arch.lifecycle.LifecycleRegistry
-
 
 
 /**
  * @author kai.w
  * @des  $des
  */
-open abstract class BaseActivity : AppCompatActivity(),LifecycleOwner {
-    private lateinit var lifecycleObserverDemo: LifecycleObserverDemo
-//    private lateinit var mLifecycleRegistry: LifecycleRegistry
+open abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
 
-//        mLifecycleRegistry = LifecycleRegistry(this)
-//        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
 
-        lifecycleObserverDemo = LifecycleObserverDemo()
-        lifecycleObserverDemo.setLifecycle(lifecycle)
     }
 
 }
